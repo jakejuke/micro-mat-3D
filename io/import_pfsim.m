d3a39e0c0f3500168be8a13f8587ac427e850ff5
Jules Dake <jules.dake@uni-ulm.de>
2023-02-07 15:58:59 +0100

    Update pf sim import code

--- io/import_pfsim.m
+++ io/import_pfsim.m
@@ -8,43 +8,55 @@ function [sim3Ds, simGTs] = import_pfsim(orimap)
 %       - path to files...
 %
 
+
 %% Import orientations
 
+% orimap = '/Users/jules/Documents/Matlab/micro-mat-3D/data/sim_import_test/orimap';
 U_list = load(orimap);
 
 % Each row of orimap represents one grain
 numGrains = size(U_list,1);
 % Assumes grains are numbered from 1 to max number of grains
-grainIDs = 1:numGrains;
+grainIDs = [1:numGrains]';
+% List for saving Rodrigues vectors
 rodList = zeros(numGrains,3);
 
+% Convert all lines of U_list to Rodrigues vectors
 for I=1:numGrains
+    
     % Create 3x3 U matrix from each row
     U =  [ U_list(I,1:3); ...
            U_list(I,4:6); ...
            U_list(I,7:9) ];
-    % Convert to Rodrigues vector
-    rodriguesVector = U2r(U);
-    % Put Rodrigues vector in fundamental zone
-    [~, ~, ~, rodList(I,:)] = mymisorientation([0 0 0], rodriguesVector);
-    %%%%%%%%%%
-    %  BUG!  %
+
+    %%%%%%%%%%%%%%%%%%%%%%
+    %  BUG! and Bug fix  %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % This misorientation between the Rodrigues vectors before and after  %
     % putting it in the fundamental zone is not zero!                     %
     % E.g., mymisorientation(rodList(1,:),rodriguesVector) != 0, bei I=1  %
+    % To fix this, I added the applysymm function.                        %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % Get minimum U (relative to ref. coordinate system) for cubic symm.
+    U_min = applysymm(U,'cubic');
+
+    % Convert to Rodrigues vector
+    rodVector = U2r(U_min);
+    % Put Rodrigues vector in fundamental zone
+    [~, ~, ~, rodList(I,:)] = mymisorientation([0 0 0], rodVector);
+
 end
 
 
-
-%% Saves current directory asks user to select data file(s)
+%% Import 3D grain ID matrices
 
 wd = pwd;
 
 % To speed up selection, you can hard code to a folder with pf sim output
-cd('/Users/jules/Library/Mobile Documents/com~apple~CloudDocs/Downloads/toJules')
+cd('/Users/jules/Documents/Matlab/micro-mat-3D/data/sim_import_test')
 
+% Ask user to select 3D data files
 [filename, pathname, filterindex] = uigetfile('*.*', ...
     'Select simulation steps to import', 'MultiSelect', 'on');
 if isequal(filename,0) || isequal(pathname,0)
@@ -57,12 +69,17 @@ elseif ischar(filename)
 else
     filenames = transpose(filename);
     filenames = sort(filenames);
+    % BUG! %
+%     % Sorting doesn't work with 1000!
+%     str = 'lm_simstep100.mat'
+%     regexp(str,'\d*','Match')
 end
 cd(pathname); clear filename
 
+% Make a basic Grain Table
 simGTs = initBasicGTs();
 
-% load 3D simdata
+% Load 3D data matrices
 sim3Ds = cell(length(filenames),1);
 for I=1:length(filenames)
     [~,varname] = fileparts(filenames{I});
@@ -77,19 +94,33 @@ end
 cd(wd)
 
 
-% simGTs = initGTs();
-% 
-% for I=1:length(sim3Ds)
-%     s = regionprops(sim3Ds{I});
-%     % simGT_bh400(I).labels = (1:length(simGT_bh400(1).labels))';
-%     simGTs(I,1).timestep = timesteps{I};
-%     simGTs(I,1).labels = fullGT.labels(vertcat(s.Area)>0);
-%     simGTs(I,1).old = fullGT.old(vertcat(s.Area)>0,:);
-%     simGTs(I,1).orient = fullGT.orient(vertcat(s.Area)>0,:);
-%     areas = vertcat(s.Area);
-%     simGTs(I,1).volume = areas(vertcat(s.Area)>0);
-%     simGTs(I,1).gradius = ((3*simGTs(I).volume)/(4*pi)).^(1/3);
-% end
+%% Set up Grain Tables
+
+for I=1:length(sim3Ds)
+    
+    % Initialize values for each timestep
+    simGTs(I,1).labels = nan(numGrains,1);
+    simGTs(I,1).orient = nan(numGrains,3);
+    simGTs(I,1).centroid = nan(numGrains,3);
+    simGTs(I,1).volume = nan(numGrains,1);
+    simGTs(I,1).gradius = nan(numGrains,1);
+    
+    % Get stats for all grains still existing for each timestep
+    s = regionprops(sim3Ds{I});
+    activeIDs = vertcat(s.Area) > 0;
+
+    % Save parameters/values for each grain at each timestep
+    simGTs(I,1).labels(activeIDs,:) = grainIDs(activeIDs);
+    simGTs(I,1).orient(activeIDs,:) = rodList(activeIDs,:);
+    simGTs(I,1).old = [simGTs(I).labels, simGTs(I).orient];
+
+    centroids = vertcat(s.Centroid);
+    simGTs(I,1).centroid(activeIDs,:) = centroids(activeIDs,:);
+
+    areas = vertcat(s.Area);
+    simGTs(I,1).volume(activeIDs,:) = areas(activeIDs,:);
+    simGTs(I,1).gradius = ((3*simGTs(I).volume)/(4*pi)).^(1/3);
+end
 
 
 end
@@ -98,8 +129,28 @@ end
 %-------------------------------------------------------------------------%
 %                        Local subfunctions                               %
 %-------------------------------------------------------------------------%
+
+% %% Do I really even need these?
+% %% Maybe I got a bit carried away with initializing variables ...
+
+
+% Initialize a basic Grain Table
+function simGTs = initBasicGTs()
+
+% fields are:
+fd00 = 'timestep';
+fd0 = 'old';
+fd1 = 'labels';
+fd2 = 'orient';
+fd3 = 'time';
+
+simGTs = struct(fd00,{},fd0,{},fd1,{},fd2,{},fd3,{});
+
+end
+
+
+% Initialize a Grain Table with more fields
 function simGTs = initGTs()
-% Initializes the grain statistics (gStats) matrix
 
 % fields are:
 fd00 = 'timestep';
@@ -111,19 +162,5 @@ fd4 = 'volume';
 fd5 = 'gradius';
 
 simGTs = struct(fd00,{},fd0,{},fd1,{},fd2,{},fd3,{},fd4,{},fd5,{});
-
-end
-
-function simGTs = initBasicGTs()
-% Initializes the grain statistics (gStats) matrix
-
-% fields are:
-fd00 = 'timestep';
-fd0 = 'old';
-fd1 = 'labels';
-fd2 = 'orient';
-fd3 = 'time';
-
-simGTs = struct(fd00,{},fd0,{},fd1,{},fd2,{},fd3,{});
 
 end
